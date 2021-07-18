650748981146e1b055f53750cd99b25aab2e8513
Ammon Johnson <ammonsawesome@gmail.com>
2021-07-17 22:54:28 -0700

    Added test code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NestedObjects
 {
@@ -6,7 +7,55 @@ namespace NestedObjects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Instructor currInst = new Instructor()
+            {
+                FullName = "Homer Simpson",
+                Email = "Homer.Simpson@cptc.edu",
+            };
+
+            Student s1 = new Student()
+            {
+                FirstName = "Jim",
+                LastName = "Halpert",
+
+            };
+
+            Student s2 = new Student()
+            {
+                FirstName = "Creed",
+                LastName = "Bratton",
+
+            };
+
+            Student s3 = new Student()
+            {
+                FirstName = "Kelly",
+                LastName = "Kapur",
+
+            };
+
+            Course test = new Course();
+            test.Title = "CPW 212 - Adv .NET";
+            test.Description = "More .NET than what you already learned";
+            test.Credits = 5;
+            test.CourseInstructor = currInst;
+            test.Roster = new List<Student>();
+
+            // Adding students to course
+            test.Roster.Add(s1);
+            test.Roster.Add(s2);
+            test.Roster.Add(s3);
+
+            Console.WriteLine(test.Title);
+            Console.WriteLine("Is taught by " + test.CourseInstructor.FullName);
+            Console.WriteLine("Credits: " + test.Credits);
+            Console.WriteLine("Currently Enrolled Students: ");
+
+            foreach(Student s in test.Roster)
+            {
+                Console.WriteLine(s.FirstName + ", " + s.LastName);
+            }
+            Console.ReadKey();
         }
     }
 }
